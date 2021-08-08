053350e098a5bfaa4219b615f6f918b58d83fac1
Mister A <andrew.tarasov1993@gmail.com>
2021-08-08 18:39:58 +0500

    fix event group endpoint

--- api/v1/configs.py
+++ api/v1/configs.py
@@ -1,20 +1,24 @@
 from flask import (
     Blueprint,
     jsonify,
-    abort
+    request
 )
-from config import calculations
-import werkzeug.exceptions
+
+from config.forecast_models import MODELS, models
+from config.models import ModelParams
 
 api = Blueprint('configs', __name__)
 
 
-@api.route('/event_groups', methods=['GET'])
-def get_available_groups():
-    groups = calculations.event_group_list()
-    return jsonify(groups)
-
-
 @api.route('/models', methods=['GET'])
 def get_available_models():
-    return jsonify(error="Not implemented"), werkzeug.exceptions.NotImplemented.code
+    return jsonify({"models": MODELS}), 200
+
+
+@api.route('/event_groups', methods=['GET'])
+def get_available_groups():
+    # TODO status ok with class
+    modelName = request.args.get('model_name')
+    selectedModel: ModelParams = models[modelName]
+    groupsList = [{"name": group.name, "alias": group.alias} for group in selectedModel.CALCULATIONS]
+    return jsonify({"groups": groupsList}), 200
