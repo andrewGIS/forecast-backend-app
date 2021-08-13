99dc06f6a04f74828da932d0bfb27233b4a895d7
Mister A <andrew.tarasov1993@gmail.com>
2021-08-13 15:35:47 +0500

    set production scheduler

--- celery_worker.py
+++ celery_worker.py
@@ -1,3 +1,4 @@
+from celery.schedules import crontab
 from app import create_app
 
 app = create_app()
@@ -13,9 +14,32 @@ from app import celery
 #         'args': ('2021072000', 'gfs')
 #     }
 # }
-celery.conf.beat_schedule = {
-    'check-zips-on-server': {
-        'task': 'app.tasks.check_new_files',
-        'schedule': 60.0
-    }
-}
+
+# TODO move switching in Env
+# dev conf
+# celery.conf.beat_schedule = {
+#     'check-zips-on-server': {
+#         'task': 'app.tasks.check_new_files',
+#         'schedule': 60.0
+#     }
+# }
+
+# prod conf date in UTC
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour=2, minute=50),
+        app.tasks.check_new_files(),
+    )
+    sender.add_periodic_task(
+        crontab(hour=4, minute=30),
+        app.tasks.check_new_files(),
+    )
+    sender.add_periodic_task(
+        crontab(hour=14, minute=50),
+        app.tasks.check_new_files(),
+    )
+    sender.add_periodic_task(
+        crontab(hour=15, minute=30),
+        app.tasks.check_new_files(),
+    )
