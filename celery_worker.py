5ba14541b3109f6f962d887cce6e411b1d8f648f
Mister A <andrew.tarasov1993@gmail.com>
2021-09-19 14:18:36 +0500

    feature: добавление загрузки по расписанию

--- celery_worker.py
+++ celery_worker.py
@@ -17,29 +17,45 @@ from app import celery
 
 # TODO move switching in Env
 # dev conf
-# celery.conf.beat_schedule = {
-#     'check-zips-on-server': {
-#         'task': 'app.tasks.check_new_files',
-#         'schedule': 60.0
-#     }
-# }
+celery.conf.beat_schedule = {
+    # 'check-zips-on-server-gfs-00': {
+    #     'task': 'app.tasks.check_new_files',
+    #     'schedule': 60.0
+    # },
+    'check-zips-on-server-gfs-00': {
+        'task': 'app.tasks.check_new_files',
+        'schedule': crontab(hour=2, minute=50)
+    },
+    'check-zips-on-server-gfs-12': {
+        'task': 'app.tasks.check_new_files',
+        'schedule': crontab(hour=4, minute=30)
+    },
+    'check-zips-on-server-icon-00': {
+        'task': 'app.tasks.check_new_files',
+        'schedule': crontab(hour=14, minute=50)
+    },
+    'check-zips-on-server-icon-12': {
+        'task': 'app.tasks.check_new_files',
+        'schedule': crontab(hour=15, minute=30)
+    },
+}
 
 # prod conf date in UTC
-@celery.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(
-        crontab(hour=2, minute=50),
-        app.tasks.check_new_files(),
-    )
-    sender.add_periodic_task(
-        crontab(hour=4, minute=30),
-        app.tasks.check_new_files(),
-    )
-    sender.add_periodic_task(
-        crontab(hour=14, minute=50),
-        app.tasks.check_new_files(),
-    )
-    sender.add_periodic_task(
-        crontab(hour=15, minute=30),
-        app.tasks.check_new_files(),
-    )
+# @celery.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     sender.add_periodic_task(
+#         crontab(hour=2, minute=50),
+#         app.tasks.check_new_files(),
+#     )
+#     sender.add_periodic_task(
+#         crontab(hour=4, minute=30),
+#         app.tasks.check_new_files(),
+#     )
+#     sender.add_periodic_task(
+#         crontab(hour=14, minute=50),
+#         app.tasks.check_new_files(),
+#     )
+#     sender.add_periodic_task(
+#         crontab(hour=15, minute=30),
+#         app.tasks.check_new_files(),
+#     )
